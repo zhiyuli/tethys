f4ec96fb40e5ca65c0e4f5b243abadf8522b7212
Zhiyu Li <zli@aquaveo.local>
2019-01-23 15:50:29 -0700

    add more inline comments

--- tethys_portal/consumers.py
+++ tethys_portal/consumers.py
@@ -7,6 +7,13 @@ from channels.generic.http import AsyncHttpConsumer
 
 class BasicHttpConsumer(AsyncHttpConsumer):
 
+    # How to test this async endpoint
+    # Open 2 blank browser tabs, run the two url at the same time
+    # http://127.0.0.1:8000/asynchttp?x=1
+    # http://127.0.0.1:8000/asynchttp?a=2
+    # check the 2 "t_start" values, the difference of which should be less than 1 second
+    # check the 2 "t_end" values, each of which should be +5 second from "t_start"
+    # check the 2 "thread_name" values that should be identical
     async def handle(self, body):
         t_start = datetime.datetime.utcnow().strftime("%H:%M:%S")
         await asyncio.sleep(5)
@@ -14,7 +21,7 @@ class BasicHttpConsumer(AsyncHttpConsumer):
         resp = dict(status="OK",
                     t_start=t_start,
                     t_end=t_end,
-                    thead_name=threading.current_thread().name)
+                    thread_name=threading.current_thread().name)
 
         await self.send_response(200, json.dumps(resp).encode(),
                                  headers=[(b"Content-Type", b"application/json"),])
