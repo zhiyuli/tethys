fca6121d9a60373390b07e291fcf7935f7512764
Zhiyu Li <zli@aquaveo.local>
2019-01-23 15:42:45 -0700

    update async test endpoint

--- tethys_portal/consumers.py
+++ tethys_portal/consumers.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import asyncio
+import threading
 from channels.generic.http import AsyncHttpConsumer
 
 
@@ -8,12 +9,12 @@ class BasicHttpConsumer(AsyncHttpConsumer):
 
     async def handle(self, body):
         t_start = datetime.datetime.utcnow().strftime("%H:%M:%S")
-        await asyncio.sleep(3)
+        await asyncio.sleep(5)
         t_end = datetime.datetime.utcnow().strftime("%H:%M:%S")
-
-        #import threading
-        #resp = threading.current_thread().name + " " + str(datetime.datetime.now())
-        resp = dict(status="OK", t_start=t_start, t_end=t_end)
+        resp = dict(status="OK",
+                    t_start=t_start,
+                    t_end=t_end,
+                    thead_name=threading.current_thread().name)
 
         await self.send_response(200, json.dumps(resp).encode(),
                                  headers=[(b"Content-Type", b"application/json"),])
